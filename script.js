b808e61f978eab85aa620f2eae1ed947784218c6
Dominiks <d.f.mekss@gmail.com>
2023-03-18 21:07:23 +0200

    Declair variables and add gather input from user
    Added 3 functions that gathers the input of players choice

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// 3 variables which contain the Rock, Paper, and Scissors
+const rock = "Rock"
+const paper = "Paper"
+const scissors = "Scissors"
+// A variable which contains players choice
+let playersChoice = ""
+// A variable which contains computers choice
+let computersChoice = ""
+
+// function that gathers the input of players choice
+function Rock() {
+    return rock
+}
+function Paper() {
+    return paper
+}
+function Scissors() {
+    return scrissors
+}
+// function that generates a random computer choice
+// function that compairs players choice and computer choice and outputs a winner
+// function that outputs text on the website depending on the previous functions output
+//
